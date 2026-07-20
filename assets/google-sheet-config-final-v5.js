31543920ab74c8dac5c77a313a378791e4218cb6
tamagay12345 <tamagay1@gmail.com>
2026-07-20 19:01:46 +0100

    Add files via upload

---
+++ assets/google-sheet-config-final-v5.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Google Apps Script Web App endpoint for consultation submissions.
+window.CONSULTATION_SHEET_ENDPOINT =
+    "https://script.google.com/macros/s/AKfycbxsbbrriCEtcShbrTUND7I66TJjsuqLZHUIw7ayia05tfmn0adhMgShMNNwBYFulEJsHQ/exec";
